88821a3f2d3ff2b5092c1d6a5d66291ddd967bcc
Ivan <upp.estancia@gmail.com>
2025-05-07 17:10:57 -0600

    Pregunta por inputs en promedio.py

--- promedio.py
+++ promedio.py
@@ -1,3 +1,9 @@
-def promedio(a,b,c):
-	return (a+b+c)/3
-print("El resultado es " promedio(1,2,3))
+def promedio(numeros):
+	suma = 0
+	return sum(numeros) / len(numeros)
+
+num1 = int(input("Escribe un número"))
+num2 = int(input("Escribe otro número"))
+num3 = int(input("Escribe un último número"))
+
+print(f"El promedio es: {promedio([num1,num2,num3])}")
